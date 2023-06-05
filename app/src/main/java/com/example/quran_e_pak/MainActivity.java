8af3ac432fbf08d453b1a9b1b93fce02cbb099d7
ibi111 <ibtsamsadiqaim@gmail.com>
2023-06-05 21:06:54 +0500

    linked launcher view components with objects

--- app/src/main/java/com/example/quran_e_pak/MainActivity.java
+++ app/src/main/java/com/example/quran_e_pak/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.quran_e_pak;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView t,scroll_view ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        t=findViewById(R.id.heading);
+        scroll_view=findViewById(R.id.editTextTextMultiLine);
+
+
     }
 }
